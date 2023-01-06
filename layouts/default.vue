396e52b1f4a3421f9f7b379a932d4fdeaf6e6563
tony140407 <a32715368@gmail.com>
2023-01-06 18:33:15 +0800

    fix: build a full-screen responsive rage with flexbox

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
     <!-- ClientOnly 避免 [Vue warn]: Hydration node mismatch -->
     <ClientOnly>
-        <section class="overflow-hidden">
+        <section class="overflow-hidden flex flex-col min-h-screen">
             <FrontendNavbar />
-            <slot></slot>
+            <div class="grow">
+                <slot></slot>
+            </div>
             <FrontendFooter />
         </section>
     </ClientOnly>
