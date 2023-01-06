80b89584d21d67a180a336e5d78812a349bc57f0
tony140407 <a32715368@gmail.com>
2023-01-06 11:10:08 +0800

    feat: add error case for getProducts

--- app.vue
+++ app.vue
@@ -1,8 +1,22 @@
 <script setup>
 import { useProductsStore } from '~/store/products'
 const { setProducts } = useProductsStore()
-const { data } = await useFetch('http://localhost:3166/api/products')
-setProducts(data.value.products)
+await useFetch('http://localhost:3166/api/products').then(
+    (res) => {
+        const data = res.data.value
+        const error = res.error.value
+        if (error) {
+            // dealing error
+            console.error(error)
+        } else {
+            setProducts(data.products)
+        }
+    },
+    (error) => {
+        console.log('exception...')
+        console.log(error)
+    },
+)
 </script>
 
 <template>
