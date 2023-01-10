eeb99af8646d5eb9f4d956e3fe2794be45356d56
tony140407 <a32715368@gmail.com>
2023-01-10 16:47:33 +0800

    feat: add selectedProduct 事件委任

--- components/Frontend/Cart/SelectedProduct.vue
+++ components/Frontend/Cart/SelectedProduct.vue
@@ -1,4 +1,5 @@
 <script setup>
+const { $swal } = useNuxtApp()
 // props & emit
 const props = defineProps({
     cart: {
@@ -7,21 +8,73 @@ const props = defineProps({
 })
 const { cart } = toRefs(props)
 
-function changeQty(num) {
-    productDetail.value.qty = num
-    // axios
-    axiosPutCartQty(num)
+function handleClick(e) {
+    const method = e.target.id // 'minus' or 'plus'
+    if (method !== 'minus' && method !== 'plus') return
+    const cartID = e.target.getAttribute('data-cartID')
+    const productID = e.target.getAttribute('data-productID')
+    const currentProduct = cart.value.find(
+        (item) => item.id == cartID && item.product_id == productID,
+    )
+    currentProduct.qty = method == 'minus' ? currentProduct.qty - 1 : currentProduct.qty + 1
+    changeQty(currentProduct, currentProduct.qty)
 
-    function axiosPutCartQty(qty) {
+    function changeQty(currentProduct, qty) {
+        console.dir({ product_id: currentProduct.product_id, qty: qty })
+        console.log(qty)
+        // const data = {
+        //     data: { product_id: currentProduct.product_id, qty: qty },
+        // }
+        // apiUpdateCart(currentProduct.id, data)
+        //     .then((res) => {
+        //         changeLoading(false)
+        //         if (res.data.success == true) {
+        //             VueSweetalert2({
+        //                 icon: 'success',
+        //                 title: '成功改變商品數量!',
+        //                 timer: 1000,
+        //                 showCloseButton: false,
+        //                 showCancelButton: false,
+        //             })
+        //             getCartData()
+        //         }
+        //     })
+        //     .catch((error) => {
+        //         changeLoading(false)
+        //         console.log(error)
+        //     })
+    }
+}
+
+function deleteSingleProduct(productDetail) {
+    $swal
+        .fire({
+            title: `你確定要移除"${productDetail.product.title}"嗎?`,
+            icon: 'error',
+            showCancelButton: true,
+            confirmButtonColor: 'rgb(239 68 68)',
+            cancelButtonColor: 'rgb(203 213 225);',
+            confirmButtonText: '確定',
+            cancelButtonText: '取消',
+            reverseButtons: true,
+        })
+        .then((result) => {
+            if (result.isConfirmed) {
+                // deleteProduct()
+                console.log('刪除成功')
+            }
+        })
+
+    function deleteProduct() {
         changeLoading(true)
-        const data = { data: { product_id: productDetail.value.product_id, qty: qty } }
-        apiUpdateCart(productDetail.value.id, data)
+        apiDeleteCart(productDetail.value.id)
             .then((res) => {
                 changeLoading(false)
                 if (res.data.success == true) {
+                    // storeData.carts
                     VueSweetalert2({
                         icon: 'success',
-                        title: '成功改變商品數量!',
+                        title: '已從購物車中刪除產品!',
                         timer: 1000,
                         showCloseButton: false,
                         showCancelButton: false,
@@ -35,35 +88,12 @@ function changeQty(num) {
             })
     }
 }
-
-function deleteSingleProduct() {
-    changeLoading(true)
-    apiDeleteCart(productDetail.value.id)
-        .then((res) => {
-            changeLoading(false)
-            if (res.data.success == true) {
-                // storeData.carts
-                VueSweetalert2({
-                    icon: 'success',
-                    title: '已從購物車中刪除產品!',
-                    timer: 1000,
-                    showCloseButton: false,
-                    showCancelButton: false,
-                })
-                getCartData()
-            }
-        })
-        .catch((error) => {
-            changeLoading(false)
-            console.log(error)
-        })
-}
 </script>
 
 <template>
-    <ul class="w-full">
+    <ul class="w-full" @click="handleClick">
         <li
-            class="mb-8 grid grid-cols-12 overflow-hidden rounded-2xl bg-gray-100"
+            class="mb-8 grid grid-cols-12 overflow-hidden rounded-2xl bg-gray-100 drop-shadow-md"
             v-for="eachProduct in cart"
             :key="eachProduct.id"
         >
@@ -95,16 +125,24 @@ function deleteSingleProduct() {
                         <div class="relative mb-4 flex items-center">
                             <input
                                 type="number"
-                                value="1"
+                                :value="eachProduct.qty"
                                 class="peer relative h-10 w-full rounded-md bg-gray-50 text-center outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:drop-shadow-lg"
                                 min="1"
+                                disabled
                             />
                             <button
-                                class="max-w-16 absolute left-0 h-10 w-1/6 rounded-l-md bg-green-400 text-xs font-semibold text-white transition-all duration-200 ease-in-out hover:bg-green-600"
+                                id="minus"
+                                :disabled="eachProduct.qty == 1"
+                                :data-cartID="eachProduct.id"
+                                :data-productID="eachProduct.product_id"
+                                class="max-w-16 absolute left-0 h-10 w-1/6 rounded-l-md bg-green-400 text-xs font-semibold text-white transition-all duration-200 ease-in-out hover:bg-green-600 disabled:bg-gray-400"
                             >
                                 -
                             </button>
                             <button
+                                id="plus"
+                                :data-cartID="eachProduct.id"
+                                :data-productID="eachProduct.product_id"
                                 class="max-w-16 absolute right-0 h-10 w-1/5 rounded-r-md bg-green-400 text-xs font-semibold text-white transition-all duration-200 ease-in-out hover:bg-green-600"
                             >
                                 +
@@ -112,6 +150,7 @@ function deleteSingleProduct() {
                         </div>
                         <button
                             class="w-full rounded-md bg-white py-1 font-bold text-red-500 outline outline-1 outline-red-500 transition-all duration-200 ease-in-out hover:bg-red-500 hover:text-white"
+                            @click="deleteSingleProduct(eachProduct)"
                         >
                             刪除
                         </button>
