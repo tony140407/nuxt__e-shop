ae5e74c64ffa9e76d1dbc21b2b806c273688f865
tony140407 <a32715368@gmail.com>
2023-01-18 10:44:56 +0800

    feat: add /admin sidebar

---
+++ layouts/admin.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- ClientOnly 避免 [Vue warn]: Hydration node mismatch -->
+    <ClientOnly>
+        <div class="flex items-stretch">
+            <AdminSidebar />
+            <section class="flex min-h-screen grow flex-col overflow-hidden">
+                <!-- <FrontendNavbar /> -->
+                <div class="grow">
+                    <slot></slot>
+                </div>
+            </section>
+        </div>
+    </ClientOnly>
+</template>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 definePageMeta({
     middleware: 'check-login',
+    layout: 'admin',
 })
 </script>
 <template>管理頁面</template>
