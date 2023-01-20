a96d17be0b31947f7e542515493eb4aff782f8f9
tony140407 <a32715368@gmail.com>
2023-01-20 21:24:51 +0800

    chore: install fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/tailwindcss'],
-    build: {
-        transpile: ['gsap'],
+  modules: [
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
+  build: {
+    transpile: ['gsap'],
+  },
+  plugins: [{ src: '~/plugins/font-awesome.ts' }],
+  css: ['~/assets/styles/sweetAlert2.css', '~/assets/styles/main.scss'],
+  runtimeConfig: {
+    public: {
+      API_KEY: process.env.API_KEY,
+      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+      DATABASE_URL: process.env.DATABASE_URL,
+      PROJECT_ID: process.env.PROJECT_ID,
+      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+      APP_ID: process.env.APP_ID,
+      MEASUREMENT_ID: process.env.MEASUREMENT_ID,
     },
-    css: ['~/assets/styles/sweetAlert2.css', '~/assets/styles/main.scss'],
-    runtimeConfig: {
-        public: {
-            API_KEY: process.env.API_KEY,
-            AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-            DATABASE_URL: process.env.DATABASE_URL,
-            PROJECT_ID: process.env.PROJECT_ID,
-            STORAGE_BUCKET: process.env.STORAGE_BUCKET,
-            MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
-            APP_ID: process.env.APP_ID,
-            MEASUREMENT_ID: process.env.MEASUREMENT_ID,
-        },
-    },
-})
+  },
+});
---
+++ plugins/font-awesome.ts
@@ -0,0 +1,26 @@
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faHospital as farHospital } from '@fortawesome/free-regular-svg-icons';
+import {
+  faCircleUser as fasCircleUser,
+  faBars as fasBars,
+  faHospital as fasHospital,
+} from '@fortawesome/free-solid-svg-icons';
+import { faDiscord as fabDiscord } from '@fortawesome/free-brands-svg-icons';
+
+// This is important, we are going to let Nuxt worry about the CSS
+config.autoAddCss = false;
+
+// You can add your icons directly in this plugin. See other examples for how you
+// can add other styles or just individual icons.
+library.add(farHospital, fasHospital, fasBars, fabDiscord);
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  vueApp.component('FontAwesomeIcon', FontAwesomeIcon);
+});
+
+// example usage:
+// <ClientOnly>
+//   <font-awesome-icon :icon="['far', 'hospital']" />
+//   <font-awesome-icon :icon="['fas', 'bars']" />
+// </ClientOnly>
