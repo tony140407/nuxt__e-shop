8b01ff5c7ac880fd5b504e5649eb57378a460904
tony140407 <a32715368@gmail.com>
2023-01-20 14:40:08 +0800

    feat: add fetchProducts in store

--- app.vue
+++ app.vue
@@ -1,32 +1,16 @@
 <script setup>
-import { useProductsStore } from '~/store/products'
-const { setProducts } = useProductsStore()
-await useFetch('/api/products').then(
-    (res) => {
-        console.log(res)
-        const data = res.data.value
-        const error = res.error.value
-        if (error) {
-            // dealing error
-            console.error(error)
-        } else {
-            setProducts(data.products)
-        }
-    },
-    (error) => {
-        console.log('exception...')
-        console.log(error)
-    },
-)
+import { useProductsStore } from '~/store/products';
+const { fetchProducts } = useProductsStore();
+await fetchProducts();
 
-const { persistUser } = useFirebaseAuth()
-persistUser()
+const { persistUser } = useFirebaseAuth();
+persistUser();
 </script>
 
 <template>
-    <div>
-        <NuxtLayout>
-            <NuxtPage />
-        </NuxtLayout>
-    </div>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
--- store/products.js
+++ store/products.js
@@ -1,74 +1,92 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 export const useProductsStore = defineStore('products', () => {
-    const products = ref([])
-    function setProducts(data) {
-        const newData = filterProducts(data)
-        products.value = newData
-        function filterProducts(product) {
-            // 資料庫刪不掉的資料
-            const excessDataIndex = product.findIndex(
-                (element) => element.title == '[賣]動物園造型衣服3',
-            )
-            if (excessDataIndex !== -1) {
-                product.splice(excessDataIndex, 1)
-            }
-
-            // 資料庫刪不掉的資料
-            // 過濾 size 資訊
-            const bracketReg = /\(*\)/ //所有有括號的
-            const sizeReg = {
-                XS: /\(+XS+\)/,
-                S: /\(+S+\)/,
-                M: /\(+M+\)/,
-                L: /\(+L+\)/,
-                XL: /\(+XL+\)/,
-                XXL: /\(+XXL+\)/,
-            }
-            const sizeList = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
-            let newProducts = []
-            // 先找出無size的樣本
-            product.filter((eachProduct) => {
-                // 沒有括號的拉出來當Card size屬性填入同名產品
-                if (!bracketReg.test(eachProduct.title)) {
-                    eachProduct.size = {
-                        XS: '',
-                        S: '',
-                        M: '',
-                        L: '',
-                        XL: '',
-                        XXL: '',
-                    }
-                    newProducts.push(eachProduct)
-                }
-            })
-            // 有括號的 ID 填入 size 屬性中
-            sizeList.filter((eachSize) => {
-                product.filter((eachProduct) => {
-                    if (sizeReg[eachSize].test(eachProduct.title)) {
-                        const key = eachProduct.title.replace(sizeReg[eachSize], '')
-                        newProducts.find((eachNewProduct) => {
-                            if (eachNewProduct.title == key) {
-                                eachNewProduct.size[eachSize] = eachProduct.id
-                                return true
-                            }
-                        })
-                    }
-                })
-            })
-            return newProducts
+  const products = ref([]);
+  async function fetchProducts() {
+    await useFetch('/api/products').then(
+      (res) => {
+        const data = res.data.value;
+        const error = res.error.value;
+        if (error) {
+          // dealing error
+          console.error(error);
+        } else {
+          setProducts(data.products);
         }
-    }
-    function filterCategory(category) {
-        // 如果沒有選擇分類，回傳全部
-        if (category == '全部') {
-            return products.value
-        }
-        return products.value.filter((eachProduct) => {
-            if (eachProduct.category == category) {
-                return true
-            }
-        })
-    }
+      },
+      (error) => {
+        console.log('exception...');
+        console.log(error);
+      }
+    );
+  }
+  function setProducts(data) {
+    const newData = filterProducts(data);
+    products.value = newData;
+    function filterProducts(product) {
+      // 資料庫刪不掉的資料
+      const excessDataIndex = product.findIndex(
+        (element) => element.title == '[賣]動物園造型衣服3'
+      );
+      if (excessDataIndex !== -1) {
+        product.splice(excessDataIndex, 1);
+      }
 
-    return { products, setProducts, filterCategory }
-})
+      // 資料庫刪不掉的資料
+      // 過濾 size 資訊
+      const bracketReg = /\(*\)/; //所有有括號的
+      const sizeReg = {
+        XS: /\(+XS+\)/,
+        S: /\(+S+\)/,
+        M: /\(+M+\)/,
+        L: /\(+L+\)/,
+        XL: /\(+XL+\)/,
+        XXL: /\(+XXL+\)/,
+      };
+      const sizeList = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+      let newProducts = [];
+      // 先找出無size的樣本
+      product.filter((eachProduct) => {
+        // 沒有括號的拉出來當Card size屬性填入同名產品
+        if (!bracketReg.test(eachProduct.title)) {
+          eachProduct.size = {
+            XS: '',
+            S: '',
+            M: '',
+            L: '',
+            XL: '',
+            XXL: '',
+          };
+          newProducts.push(eachProduct);
+        }
+      });
+      // 有括號的 ID 填入 size 屬性中
+      sizeList.filter((eachSize) => {
+        product.filter((eachProduct) => {
+          if (sizeReg[eachSize].test(eachProduct.title)) {
+            const key = eachProduct.title.replace(sizeReg[eachSize], '');
+            newProducts.find((eachNewProduct) => {
+              if (eachNewProduct.title == key) {
+                eachNewProduct.size[eachSize] = eachProduct.id;
+                return true;
+              }
+            });
+          }
+        });
+      });
+      return newProducts;
+    }
+  }
+  function filterCategory(category) {
+    // 如果沒有選擇分類，回傳全部
+    if (category == '全部') {
+      return products.value;
+    }
+    return products.value.filter((eachProduct) => {
+      if (eachProduct.category == category) {
+        return true;
+      }
+    });
+  }
+
+  return { products, fetchProducts, filterCategory };
+});
