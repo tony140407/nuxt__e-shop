9cfc3d3c7c0e02e3ca5275dd2407f1424c69da13
tony140407 <a32715368@gmail.com>
2023-01-23 13:00:32 +0800

    feat: add env variable

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt',
-    '@nuxtjs/tailwindcss',
-  ],
-  build: {
-    transpile: ['gsap'],
-  },
-  plugins: [{ src: '~/plugins/font-awesome.ts' }],
-  css: ['~/assets/styles/sweetAlert2.css', '~/assets/styles/main.scss'],
-  runtimeConfig: {
-    public: {
-      API_KEY: process.env.API_KEY,
-      AUTH_DOMAIN: process.env.AUTH_DOMAIN,
-      DATABASE_URL: process.env.DATABASE_URL,
-      PROJECT_ID: process.env.PROJECT_ID,
-      STORAGE_BUCKET: process.env.STORAGE_BUCKET,
-      MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
-      APP_ID: process.env.APP_ID,
-      MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+    modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@nuxtjs/tailwindcss'],
+    build: {
+        transpile: ['gsap'],
     },
-  },
-});
+    plugins: [{ src: '~/plugins/font-awesome.ts' }],
+    css: ['~/assets/styles/sweetAlert2.css', '~/assets/styles/main.scss'],
+    runtimeConfig: {
+        public: {
+            VUE_APP_baseUrl: process.env.VUE_APP_baseUrl,
+            VUE_APP_apiKey: process.env.VUE_APP_apiKey,
+            API_KEY: process.env.API_KEY,
+            AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+            DATABASE_URL: process.env.DATABASE_URL,
+            PROJECT_ID: process.env.PROJECT_ID,
+            STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+            MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+            APP_ID: process.env.APP_ID,
+            MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+        },
+    },
+})
